bfee5ca3f9c80d2b3dbfa2219e31e7adc0a116ae
yavuzselimozkok <yavuzselimozkok@gmail.com>
2026-07-12 23:00:02 +0300

    kelime vadisi

---
+++ mevcut_kelime.py
@@ -0,0 +1,7 @@
+with open("../KelimeListesi/Turkce-Kelime-Listesi/turkce_kelime_listesi.txt",
+          encoding="utf-8") as f:
+    mevcut_kelime = set(line.strip() for line in f)
+
+print(f"{len(mevcut_kelime)} mevcut kelime sayisidir")
+
+print(mevcut_kelime)
